5a356cc22d5b7d3ba5b6bdaba3a9e8f8dfac7abb
Elias Santistevan <elias.santistevan@sparkfun.local>
2019-07-03 08:25:06 -0600

    Swaps file names

--- newLib.cpp
+++ newLib.cpp
@@ -1,44 +1,100 @@
-#ifndef _CLASS_NAME_H
-#define _CLASS_NAME_H
+/*
+  This is a library for...
+  By: Elias Santistevan
+  Date: 
+  License: This code is public domain but you buy me a beer if you use this and 
+  we meet someday (Beerware license).
 
-#include <Wire.h>
-#include <SPI.h>
-#include <Arduino.h>
+  Feel like supporting our work? Buy a board from SparkFun!
+ */
 
-typedef uint16_t i2cAddress;
-const i2cAddress defAddr = 0x00;
+#include "Class_Name.h"
 
-enum {
-};
+Class_Name::Class_Name(){} //Constructor for SPI
+Class_Name::Class_Name(uint16_t address){  _address = address; } //Constructor for I2C
 
-class Class_Name
-{  
-  public:
+bool Class_Name::begin( TwoWire &wirePort )
+{
+  
+  _i2cPort = &wirePort;
 
-    className constructor(); // SPI Constructor
-    className constructor(uint16_t address); // I2C Constructor
+  _i2cPort->beginTransmission(_address);
+  uint8_t _ret = _i2cPort->endTransmission();
+  if( !_ret )
+    return true; 
+  else 
+    return false; 
 
-    bool begin( TwoWire &wirePort );
-    bool beginSpi(uint8_t userCsPin, SPIClass &spiPort);
+}
 
-    bool begin(TwoWire &wirePort = Wire); // begin function
-    bool beginSpi(SPIClass &spiPort = SPI); 
+bool Class_Name::beginSpi(uint8_t userCsPin, SPIClass &spiPort)
+{
+  _i2cPort = NULL;
 
-  private:
+  _spiPort = &spiPort;
 
-    uint16_t _address;
+  _cs = userCsPin;
+  pinMode(_cs, OUTPUT);
+  digitalWrite(_cs, HIGH);
 
-    // This generic function reads an eight bit register. It takes the register's
-    // address as its' parameter. 
-    uint8_t readRegister(uint8_t _reg);
-
-    // This generic function handles I2C write commands for modifying individual
-    // bits in an eight bit register. Paramaters include the register's address, a mask 
-    // for bits that are ignored, the bits to write, and the bits' starting
-    // position.
-    void writeRegister(uint8_t _wReg, uint8_t _mask, uint8_t _bits, uint8_t _startPosition);
-
-    TwoWire *_i2cPort;
-    SPIClass *_spiPort;
-};
+  _spiPort.begin();
+#ifdef ESP32 
+    SPI.setBitOrder(SPI_MSBFIRST);
+#else
+    SPI.setBitOrder(MSBFIRST);
 #endif
+
+  return true; 
+}
+
+// This generic function handles I2C write commands for modifying individual
+// bits in an eight bit register. Paramaters include the register's address, a mask 
+// for bits that are ignored, the bits to write, and the bits' starting
+// position.
+void Class_Name::writeRegister(uint8_t _wReg, uint8_t _mask, uint8_t _bits, uint8_t _startPosition)
+{
+  if(_i2cPort == NULL) {
+    _spiWrite = readRegister(_wReg); // Get the current value of the register
+    _spiWrite &= (~_mask); // Mask the position we want to write to
+    _spiWrite |= (_bits << _startPosition); // Write the given bits to the variable
+    _spiPort->beginTransaction(SPISettings(_spiPortSpeed, MSBFIRST, SPI_MODE1)); 
+    digitalWrite(_cs, LOW); // Start communication
+    _spiPort->transfer(_wReg); // Start write command at given register
+    _spiPort->transfer(_spiWrite); // Write to register
+    digitalWrite(_cs, HIGH); // End communcation
+    _spiPort->endTransaction();
+  }
+  else { 
+    _i2cWrite = readRegister(_wReg); // Get the current value of the register
+    _i2cWrite &= (~_mask); // Mask the position we want to write to.
+    _i2cWrite |= (_bits << _startPosition);  // Write the given bits to the variable
+    _i2cPort->beginTransmission(_address); // Start communication.
+    _i2cPort->write(_wReg); // at register....
+    _i2cPort->write(_i2cWrite); // Write register...
+    _i2cPort->endTransmission(); // End communcation.
+  }
+}
+
+// This generic function reads an eight bit register. It takes the register's
+// address as its' parameter. 
+uint8_t Class_Name::readRegister(uint8_t _reg)
+{
+
+  if(_i2cPort == NULL) {
+    _spiPort->beginTransaction(SPISettings(_spiPortSpeed, MSBFIRST, SPI_MODE1)); 
+    digitalWrite(_cs, LOW); // Start communication.
+    _spiPort->transfer(_reg |= SPI_READ_M);  // Register OR'ed with SPI read command. 
+    _regValue = _spiPort->transfer(0); // Get data from register.  
+    _spiPort->endTransaction();
+    return(_regValue); 
+  }
+  else {
+    _i2cPort->beginTransmission(_address); 
+    _i2cPort->write(_reg); // Moves pointer to register.
+    _i2cPort->endTransmission(false); // 'False' here sends a restart message so that bus is not released
+    _i2cPort->requestFrom(_address, 1); // Read the register, only ever once. 
+    _regValue = _i2cPort->read();
+    return(_regValue);
+  }
+}
+
